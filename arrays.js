c902223089228af3e8d37e100cfd4c6931e2d63b
Salvatore Santamaria <sam.santamaria@gmail.com>
2020-09-01 15:53:20 -0700

    added swapTowardsCenter

--- arrays.js
+++ arrays.js
@@ -25,3 +25,14 @@ function arrayLessThan100(arr) {
 function findSmallestNum(arr) {
 	return arr.sort((a,b) => a - b)[0]
 }
+
+// Create a function that takes an array and swaps all the numbers towards the center.
+function swapTowardCenter(arr) {
+
+	for (var i = 0; i < arr.length/2; i++) {
+	var temp = arr[i];
+	arr[i] = arr[arr.length - 1 - i]
+	arr[arr.length - 1 - i] = temp;
+	}
+	return arr
+}
