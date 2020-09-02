75296e41220325cbe9ad801d344976c3351ab243
Salvatore Santamaria <sam.santamaria@gmail.com>
2020-09-02 12:18:00 -0700

    Added reverse array without .reverse()

--- arrays.js
+++ arrays.js
@@ -51,3 +51,14 @@ function threesFives(num){
 }
 
 threesFives(10);
+
+// Given a numerical array, reverse the order of values, in-place without using .reverse()
+function reverse(arr){
+//return arr.reverse()
+	for (let i = 0; i < (arr.length- 1) / 2; i++) {
+		let el = arr[i];
+		arr[i] = arr[arr.length - 1 - i];
+		arr[arr.length - 1 - i] = el;
+	}
+	return arr;
+|}
