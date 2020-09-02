38f13266b00eb2e582b49ee626538a11fa1dc35c
Salvatore Santamaria <sam.santamaria@gmail.com>
2020-09-01 20:43:07 -0700

    Added threesfives

--- arrays.js
+++ arrays.js
@@ -36,3 +36,18 @@ function swapTowardCenter(arr) {
 	}
 	return arr
 }
+
+// Create threesFives(n) that adds values from 1 and n (inclusive) if that value is not divisible by 3 or 5. Return the final sum.
+// For example, threesFives(10) returns 22 as it only returns the sum of 1, 2, 4, 7, and 8. On that example, it skips 3, 6, 9 as those are divisible by 3. It also skips 5, and 10 as it's divisible by 5.
+function threesFives(num){
+	let result = 0;
+	for (var i = 0; i < num; i++) {
+		if (i % 5 == 0 || i % 3 == 0) {  
+		} else {
+			result = result + i;
+		}
+	}
+	return result;
+}
+
+threesFives(10);
