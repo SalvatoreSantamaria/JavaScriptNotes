dd3cbc262eff78d765d67f1799f96b7f9bb09b63
Salvatore Santamaria <sam.santamaria@gmail.com>
2020-09-13 14:46:26 -0700

    Fix 2D Array - DS negative test cases

--- arrays.js
+++ arrays.js
@@ -81,7 +81,7 @@ function hourglassSum(arr) {
 
 	let rows = arr.length //all of the arr
 	let columns = arr[0].length //the number of columns is calculated via the length of the first line
-	let max_sum = 0
+	let max_sum = -63 // set this to -63 to handle negative arr value test cases
 
 	// loops need to stop at (rows - 2) and (columns -2) because that's how we prevent the hourglass from 
 	// going outside of the grid matrix
