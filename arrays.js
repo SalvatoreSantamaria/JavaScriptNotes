5c35655500048eec93f4c787d7dda99a27cdcedb
Salvatore Santamaria <sam.santamaria@gmail.com>
2020-09-13 14:35:20 -0700

    Added Hackerrank 2D Array DS

--- arrays.js
+++ arrays.js
@@ -72,5 +72,36 @@ function rotLeft(arr, dRotations) {
   const rightSide = arr.splice(0, dRotations)
   console.log(arr, rightSide)
   return arr.concat(rightSide);
+}
 
+// Hackerrank
+// 2D Array - DS
+
+function hourglassSum(arr) {
+
+	let rows = arr.length //all of the arr
+	let columns = arr[0].length //the number of columns is calculated via the length of the first line
+	let max_sum = 0
+
+	// loops need to stop at (rows - 2) and (columns -2) because that's how we prevent the hourglass from 
+	// going outside of the grid matrix
+
+	// 1 1 1 0 0 0
+	// 0 1 0 0 0 0
+	// 1 1 1 0 0 0
+	// 0 0 2 4 4 0
+	// 0 0 0 2 0 0
+	// 0 0 1 2 4 0
+	//output should be 19
+
+	for (let i = 0; i < rows-2; i++) {
+			for (let j = 0; j < columns-2; j++) {
+					let current_sum = 
+						arr[i][j] + arr[i][j+1] + arr[i][j+2] + 
+						arr[i+1][j+1] +
+						arr[i+2][j] + arr[i+2][j+1] + arr[i+2][j+2]
+					max_sum = Math.max(max_sum, current_sum) //take whichever is bigger
+			}
+	}
+	return max_sum
 }
