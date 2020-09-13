16a9bb09bd859474b3c6d93f893f07aed8c4f7c4
Salvatore Santamaria <sam.santamaria@gmail.com>
2020-09-13 08:32:07 -0700

    Arrays Left Rotation

--- arrays.js
+++ arrays.js
@@ -63,4 +63,14 @@ function reverse(arr){
 	return arr;
 }
 
+// Hackerrank
+// Rotate an array left the amount of dRotations
+// arr = [1, 2, 3, 4, 5]
+// dRotations = 4 
+// returns 5 1 2 3 4
+function rotLeft(arr, dRotations) {
+  const rightSide = arr.splice(0, dRotations)
+  console.log(arr, rightSide)
+  return arr.concat(rightSide);
 
+}
