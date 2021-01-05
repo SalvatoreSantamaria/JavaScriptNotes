1d38957b9ba2d643a25d7fb8e710f76e2c480454
Salvatore Santamaria <sam.santamaria@gmail.com>
2021-01-05 14:29:11 -0800

    Added map

--- ES6-JavaScript-The-Complete-Developers-Guide/map.js
+++ ES6-JavaScript-The-Complete-Developers-Guide/map.js
@@ -1,3 +1,5 @@
+//Map returns new array
+
 let numbers = [1,2,3,4]
 
 let doubles = numbers.map((n) => {
@@ -6,3 +8,15 @@ let doubles = numbers.map((n) => {
 
 console.log(numbers)
 console.log(doubles)
+
+let cars = [
+  { model: 'Escape', year: 2005 },
+  { model: 'Escalade', year: 2005 },
+  { model: 'F-250', year: 1999 }
+]
+
+//return array of years
+let carYear = cars.map((car) => {
+  return car.year
+})
+console.log(carYear)
