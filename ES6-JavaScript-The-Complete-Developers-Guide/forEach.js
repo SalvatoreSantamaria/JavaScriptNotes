936f27f193680c2917f5cc783a6ad9ae51529426
Salvatore Santamaria <sam.santamaria@gmail.com>
2021-01-11 19:55:59 -0800

    Added forEach notes

--- ES6-JavaScript-The-Complete-Developers-Guide/forEach.js
+++ ES6-JavaScript-The-Complete-Developers-Guide/forEach.js
@@ -20,3 +20,16 @@ function logger(input) {
   console.log('function logger is logging with for each! input is: ' + input)
 }
 letters.forEach(logger)
+
+
+// forEach works great to call another function to each element in an array
+let letters2 = ['a', 'b', 'c']
+let output = (item, index, array) => {
+  console.log(item, index )
+}
+letters2.forEach(output) //logs a 0  b 1 c 2
+//OR
+letters2.forEach((item, index, array)=> {
+  item = item.toUpperCase() //just overwriting item here
+  console.log('this is with an anonymous function ' + item, index)
+})
