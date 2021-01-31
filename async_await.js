e3480bc4390241adb961f6807e91fa42ade81d2a
Salvatore Santamaria <sam.santamaria@gmail.com>
2021-01-31 08:24:07 -0800

    added try and catch basic example

--- async_await.js
+++ async_await.js
@@ -24,3 +24,18 @@ async function someFunction2() {
     return data
 }
 someFunction2().then(users => console.log(users)) // logs the users data
+
+//----------------------------------------------------------------------------------
+// Use Await for promises inside these async functions. Using Try
+async function someFunction3() {
+    try {
+        let url = 'badUrl'
+        let response = await fetch(url) // awaits response of fetch call
+        let data = await response.json() //awaits again
+        console.log('This will log BEFORE the awaits. Getting user...')
+        return data
+    } catch(err) {
+        console.log('there has been an error' + err)
+    }
+}
+someFunction3().then(users => console.log(users)) // logs the users data
