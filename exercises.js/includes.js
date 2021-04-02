c08153a66ac5e7bfdfb7ac927a50276416b9df2c
Salvatore Santamaria <salvatore.santamaria@gopuff.com>
2021-04-02 11:59:04 -0700

    Added unary operator

---
+++ exercises.js/includes.js
@@ -0,0 +1,6 @@
+//Real Work Use
+const userId = 100
+const allowedUser = [100, 101, 102, 103].includes(userId)
+
+
+
---
+++ exercises.js/map_exercises.js
@@ -0,0 +1,22 @@
+// Map returns new array of items
+// Use someArr.map((item, index, array) => {
+//   return () //must include return statement
+// }
+
+const cars = [
+  { id: 1, make: 'Ford', model: 'Escape', year: 2020 },
+  { id: 2, make: 'Chevrolet', model: 'Escalade', year: 2005 },
+  { id: 3, make: 'Ford', model: 'F-150', year: 2021 },
+  { id: 4, make: 'Ford', model: 'F-250', year: 1990 },
+  { id: 5, make: 'Ford', model: 'F-150', year: 1970 }
+]
+
+// return array of years
+// return cars that are older than 2000
+
+
+const numbers = [0,10,22,33,404,500,666,7,.8,99]
+
+// return array of even numbers
+// return array of numbers larger that 20
+
