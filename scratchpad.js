186636ae0f2669b81d332d1e032e5bca05c644cb
Salvatore Santamaria <salvatore.santamaria@gopuff.com>
2021-09-27 06:56:11 -0700

    Added JS Algo Basics, started CSS basics

--- scratchpad.js
+++ scratchpad.js
@@ -1,27 +1,12 @@
-//console.log('hello')
-//reminder: 0 false, 1 true
-
-let list = ['puff', 'jerk', 'dumbass']
-const isProfane = (profanity_list, test_input) => {
-    return profanity_list.filter(word => {
-      const regExp = new RegExp(`(\\b|_)${word.replace(/(\W)/g, '\\$1')}(\\b|_)`, 'gi')
-      ///console.log(regExp.test(test_input))returning false
-      return regExp.test(test_input)
-    }).length //returns either 0, or 1(if true)
-  }
+let expense = {
+  type: 'stuff',
+  value: 1,
+}
 
 
 
-  console.log(isProfane(list, 'letspuff'))
+const someFunction = ({value, type}) => {
+  console.log(value, type)
+}
 
-  /////
-
-// The test() method executes a search for a match between a regular expression and a specified string. 
-// Returns true or false 
-
-`(\\b|_)${word.replace(/(\W)/g, '\\$1')}(\\b|_)`, 'gi'
-// filter only returns whats true
-//unsure      (\\b|_) match b char OR _ 
-//      /(\W)/g global search for not word
-//unsure     '\\$1' $1 capture group
-//      gi global and ignore case
+someFunction(expense)
