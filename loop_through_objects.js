13e97c11478a11fb7e18c6314c85f00b902aedb0
Salvatore Santamaria <salvatore.santamaria@gopuff.com>
2021-10-09 07:25:49 -0700

    Added some quick CSS notes

---
+++ loop_through_objects.js
@@ -0,0 +1,9 @@
+// How to loop through objects in JavaScript?
+// The various methods that can be used to loop through objects in JavaScript are:
+
+// Using a for...in loop
+// Object.keys method
+// Object.values method
+// Object.entries method
+
+// https://flexiple.com/loop-through-object-javascript/
