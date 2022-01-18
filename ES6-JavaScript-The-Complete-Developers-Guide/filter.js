ad3658eebcdcb28f5b16dd12545bdd3caf27d471
Salvatore Santamaria <salvatore.santamaria@gopuff.com>
2022-01-18 07:39:34 -0800

    updated filter

--- ES6-JavaScript-The-Complete-Developers-Guide/filter.js
+++ ES6-JavaScript-The-Complete-Developers-Guide/filter.js
@@ -20,7 +20,7 @@ let IPAsInStock = beers.filter((b)=> {
   return(b.type == "IPA" && b.qty > 0) 
 })
 console.log(IPAsInStock)
-
+// _____________________________________________________________________
 //real world:
 let post = {id: 4, title: 'Post 4'};
 let comments = [
@@ -36,3 +36,24 @@ function postComments(post, comments) {
   })
 }
 console.log(postComments(post, comments))
+
+// _____________________________________________________________________
+//another real world:
+
+let items = [
+  { id: 1, name: 'One', color: 'red' },
+  { id: 2, name: 'Two', color: 'blue' },
+  { id: 3, name: 'Three', color: 'black' },
+  { id: 4, name: 'Four', color: 'black' },
+  { id: 5, name: 'Five', color: 'black' }
+]
+
+
+//resources[0].plans[0].perksList = resources[0].plans[0].perksList.filter(perk => perk.key !== perkkey);
+items = items.filter(i => i.color !== 'black')
+console.log('items is ', items)
+// logs
+// items is  [
+//   { id: 1, name: 'One', color: 'red' },
+//   { id: 2, name: 'Two', color: 'blue' }
+// ]
