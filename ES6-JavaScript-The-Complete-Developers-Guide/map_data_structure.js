c6c1754d6ab815529b6d93877010df3b3dca7e65
Salvatore Santamaria <sam.santamaria@gmail.com>
2022-08-02 21:06:57 -0700

    Map Data Structure Notes

--- ES6-JavaScript-The-Complete-Developers-Guide/map_data_structure.js
+++ ES6-JavaScript-The-Complete-Developers-Guide/map_data_structure.js
@@ -23,7 +23,7 @@ let myMap = new Map();
 myMap.set("key", "value")
 myMap.set(1, true)
 myMap.set({}, "object value")
-console.log(myMap) //Map { 'key' => 'value', 1 => true, {} => 'object value' }
+//console.log(myMap) //Map { 'key' => 'value', 1 => true, {} => 'object value' }
 
 let myMap2 = new Map([
   [ "key", "value" ],
@@ -32,23 +32,29 @@ let myMap2 = new Map([
   [ new Date(), "today" ],
   [ 2, { something: ['one', 'two', 'three' ] }]
 ])
-console.log(myMap2)
+//console.log('myMap2 ', myMap2)
+// Map {
+//   'key' => 'value',
+//   1 => true,
+//   {} => 'object value',
+//   2022-07-29T21:32:14.981Z => 'today',
+//   2 => { something: [ 'one', 'two', 'three' ] }
 
 // Map vs Object /////////////////////////////////////////////////////////////////////////
 let dog = new Map()
 dog.set('name', 'Kuma')
 dog.set(1, true) //dont use dog[1] because it will make it a string
 dog.set({}, 'some object')
-console.log(dog.get(1)) // can't access keys with '.' notation. dog.1 wouldn't work, neither would dog.name 
+//console.log(dog.get(1)) // can't access keys with '.' notation. dog.1 wouldn't work, neither would dog.name 
 
 let dog2 = new Object()
 dog2.one = true //can't do dog2.1
-console.log(dog2.one)
-console.log(dog2)
+//console.log(dog2.one)
+//console.log(dog2)
 
-console.log(dog) //Map { 1 => true, {} => 'some object' }
+//console.log(dog) //Map { 1 => true, {} => 'some object' }
 let obj = Object.fromEntries(dog.entries()) 
-console.log(obj) //{ '1': true, '[object Object]': 'some object' }
+//console.log(obj) //{ '1': true, '[object Object]': 'some object' }
 
 // Iterating over Map /////////////////////////////////////////////////////////////////////////
 // .keys //returns iterable for keys
@@ -62,17 +68,96 @@ let priceMap = new Map([
 ])
 
 for (let vegetable of priceMap.keys()) {
-  console.log(vegetable) //cucumber, lettuce, carrots
+  //console.log(vegetable) //cucumber, lettuce, carrots
 }
 
 for (let price of priceMap.values()) {
-  console.log(price) //500, 350, 50
+  //console.log(price) //500, 350, 50
 }
 
 for (let entry of priceMap.entries()) {
-  console.log(entry) // [ 'cucumber', 500 ][ 'lettuce', 350 ][ 'carrots', 50 ]
+  //console.log(entry) // [ 'cucumber', 500 ][ 'lettuce', 350 ][ 'carrots', 50 ]
 }
 
 priceMap.forEach( (value, key, map) => {
-  console.log(value, key, map)
+  //console.log(value, key, map)
 })
+
+//Converting an Object into a Map ----------------------------------------------------------------------
+
+const lettersObject = {
+  a: 'first', 
+  b: 'second', 
+  c: 'third'
+}
+// for (let i in myObject) {
+//   console.log('1', i) // logs a b c
+//   console.log('2', object[i]) // logs first second third
+// }
+
+const lettersMap = new Map(Object.entries(lettersObject))
+console.log('lettersMap ', lettersMap)
+
+//You can also convert like this, in case some key/values need to be modified in the creation.
+const objectToMap = obj => {
+  const keys = Object.keys(obj);
+  const map = new Map();
+  for(let i = 0; i < keys.length; i++){
+     //inserting new key value pair inside map
+     map.set(keys[i], obj[keys[i]]);
+  };
+  return map;
+};
+console.log(objectToMap(lettersObject));
+
+//Converting an Array into a Map ----------------------------------------------------------------------
+const letters = ['a', 'b', 'c', 'd']
+// for (let i in letters) { 
+//   console.log('3', i) // logs 0 1 2 3 
+//   console.log('4 ', letters[i]) // logs a b c d // can also use for of to get values instead of position.
+// }
+
+// One way to do this
+var test = letters.reduce(function(map, obj) {
+  console.log('obj ', obj)
+  map[obj.key] = obj.val;
+  return map;
+}, {});
+console.log('test ', test)
+
+// Another way to do this:
+const nums = [4,1,2,1,2]
+var singleNumber = function(nums) { 
+  let map = new Map()
+  for (let i = 0; i < nums.length; i++) {
+      map.set(i, nums[i])
+  }
+  console.log('map ', map)  // logs { 0 => 4, 1 => 1, 2 => 2, 3 => 1, 4 => 2 }
+}
+
+
+//Converting an Array of Objects into a Map
+//----------------------------------------------------------------------
+var arrOfObj = [
+  { key: 'foo', val: 'bar' },
+  { key: 'hello', val: 'world' }
+];
+
+var result = arrOfObj.reduce(function(map, obj) {
+  map[obj.key] = obj.val;
+  return map;
+}, {});
+
+console.log(result);
+// { foo:'bar', hello:'world' }
+
+
+//----------------------------------------------------------------------
+// Adding in values to Map values
+let classes = []
+if (!map.has(enrollmentArray[i][0])) {
+  classes.push(enrollmentArray[i][1])
+  map.set(enrollmentArray[i][0], classes)
+} else {
+  map.set(enrollmentArray[i][0], [...map.get(enrollmentArray[i][0]), enrollmentArray[i][1]])
+}
