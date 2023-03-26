defee0cc94178110b1ef350ac95f3beb7977c909
Salvatore Santamaria <sam.santamaria@gmail.com>
2023-03-26 14:36:25 -0700

    Added

--- Recursion/1recursion_pattern_cheatsheet_and_guide.js
+++ Recursion/1recursion_pattern_cheatsheet_and_guide.js
@@ -10,29 +10,46 @@
 
 // USE THIS PATTERN! 
 // Logging out all of the elements in an array with the helper pattern recursion
-let helperBasicRecursion = (arr) => {
+
+let helperBasicRecursion = (arr) => {// Path 1
   //let result = [] //collect all the elements if needed
   let helper = (helperInputArr) => {
 
-    if (helperInputArr.length === 0) { //neccessary to know when to end
-      return 
+    if (helperInputArr.length === 0) {//Path 3 and Path 6 neccessary to know when to end
+      return //
     }
 
     // if (something) { //logic manipulation
     //   arr.push, etc
     // }
 
-    console.log(helperInputArr[0])
-    helper(helperInputArr.slice(1)) //recursive call itself, with the smaller, sliced version of the arr
+    console.log(helperInputArr[0])//Path 4 
+    helper(helperInputArr.slice(1)) //Path 5 //recursive call itself, with the smaller, sliced version of the arr
   }
-  helper(arr)
+  helper(arr)//Path 2
   //return result
 }
-
 helperBasicRecursion([10,11,12])
-//To do- write out the pattern this will perform logic in, like https://www.udemy.com/course/js-algorithms-and-data-structures-masterclass/learn/lecture/10793654#overview
 
-//Something like 
-// helperBasicRecursion([10, 11, 13])
-//   helperBasicRecursion([11, 13])
-//     helperBasicRecursion([13])
+
+// A basic example that returns all the odd values in an array using recursion with a helper method
+function collectOddValues(arr){
+  let result = [];// scoped outside of helper function to make recursion easier.
+
+  function helper(helperInput){
+      if(helperInput.length === 0) { //1
+          return;//5 returns if we are out of arr elements
+      }
+      
+      if(helperInput[0] % 2 !== 0){ //2 //check if even
+          result.push(helperInput[0])//3 //only add into result if even
+      }
+      
+      helper(helperInput.slice(1))//4 pass in smaller array to recursive function
+  }
+  helper(arr)
+
+  return result;// 6 returns the result array. 
+  // The helper method style of recursion is easier to understand.
+}
+collectOddValues([1,2,3,4,5,6,7,8,9])
